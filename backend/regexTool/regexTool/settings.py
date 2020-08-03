86edfc9bfbc5013999ced5e910b047d1dd5b144a
sagi <sagikarni4@gmail.com>
2020-08-03 22:15:18 +0300

    Updated static files

--- backend/regexTool/regexTool/settings.py
+++ backend/regexTool/regexTool/settings.py
@@ -134,7 +134,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'https://localhost:3000',
     'http://localhost:3000',
-    'http://localhost:8000',
-    'https://localhost:8000'
+    'http://127.0.0.1:8000',
+    'https://127.0.0.1:8000'
 )
 
